8b00b05bc1195e47d27ec80f05f8436d7a3f1556
militaogui <>
2021-05-08 23:49:27 +0000

    lista de laços

---
+++ SEMANA09_q17.c
@@ -0,0 +1,83 @@
+/*#include <stdio.h>
+int main(){
+
+int zero, um, dois, tres, quatro, cinco, seis, sete, oito, nove,valor;
+
+  zero = 0;
+  um = 0;
+  dois = 0;
+  tres = 0;
+  quatro = 0;
+  cinco = 0;
+  seis = 0;
+  sete = 0;
+  oito = 0 ;
+  nove = 0;
+
+do{
+  scanf("%i",&valor);
+
+  if(valor==0){
+    zero++;
+   }
+  if(valor==1){
+    um++ ;
+  }
+  if(valor==2){
+    dois++ ;
+  }
+  if(valor==3){
+    tres++;
+  } 
+  if(valor==4){
+    quatro++;
+  }   
+  if(valor==5){
+    cinco++;
+  }   
+  if(valor==6){
+    seis++;
+  }   
+  if(valor==7){
+    sete++;
+  }   
+  if(valor==8){
+    oito++;
+  }   
+   if(valor==9){
+    nove++;
+  }
+}while(valor>=0);
+
+if(zero>0){
+    printf("0:%i\n", zero);
+    }
+if(um>0){
+    printf("1:%i\n", um);
+    }
+if(dois>0){
+    printf("2:%i\n", dois);
+    }
+if(tres>0){
+    printf("3:%i\n", tres);
+    }
+if(quatro>0){
+    printf("4:%i\n", quatro);
+    }
+if(cinco>0){
+    printf("5:%i\n", cinco);
+    }
+if(seis>0){
+    printf("6:%i\n", seis);
+    }
+if(sete>0){
+    printf("7:%i\n", sete);
+    }
+if(oito>0){
+    printf("8:%i\n", oito);
+  }
+if(nove>0){
+    printf("9:%i\n", nove);
+    }
+  return 0;
+}*/
